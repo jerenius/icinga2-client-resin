4eb92aaff2ae6164f615088c26c4ac79ad2dedb1
Jere <jerenius@gmail.com>
2017-03-12 14:31:26 +0200

    foo

--- start.sh
+++ start.sh
@@ -4,3 +4,10 @@
 
 curl -k -s -u tg-icinga2-client-ticket:asejkgh43734asuygsaa -H 'Accept: application/json' \
  -X POST 'https://$MASTER_HOST:5665/v1/actions/generate-ticket' -d '{ "cn": "CLIENT_IP"}'
+
+
+#apt-get update
+#apt-get install -y icinga2 monitoring-plugins monitoring-plugins-basic monitoring-plugins-common monitoring-plugins-standard
+#apt-get clean
+#rm -rf /var/lib/apt/lists/*
+
